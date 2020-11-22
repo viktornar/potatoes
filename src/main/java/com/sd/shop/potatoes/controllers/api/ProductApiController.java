807ef30b2df35c58638226f7d9108cc285413e52
Viktor Nareiko <vnareiko.lt@gmail.com>
2020-11-22 10:45:13 +0200

    Implementing query method example.

--- src/main/java/com/sd/shop/potatoes/controllers/api/ProductApiController.java
+++ src/main/java/com/sd/shop/potatoes/controllers/api/ProductApiController.java
@@ -4,6 +4,7 @@ import com.sd.shop.potatoes.entities.Product;
 import com.sd.shop.potatoes.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -18,5 +19,10 @@ public class ProductApiController {
     @GetMapping("/api/products")
     List<Product> getProducts() {
         return (List<Product>) Optional.of(productRepository.findAll()).orElse(new ArrayList<>());
+    }
+
+    @GetMapping("/api/products/query")
+    List<Product> getProductsByQueryMethod(@RequestParam String name, @RequestParam Double price) {
+        return (List<Product>) Optional.of(productRepository.findByNameAndPriceGreaterThan(name, price)).orElse(new ArrayList<>());
     }
 }
--- src/main/java/com/sd/shop/potatoes/repositories/ProductRepository.java
+++ src/main/java/com/sd/shop/potatoes/repositories/ProductRepository.java
@@ -4,6 +4,11 @@ import com.sd.shop.potatoes.entities.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
+    Iterable<Product> findByNameAndPriceGreaterThan(String name, Double price);
+
+    // Turit rasti pagal pavadinima arba pagal kaina didesne nei arba kiekis didesnis nei
 }
