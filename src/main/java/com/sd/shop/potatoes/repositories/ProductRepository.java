5e5f7891a5fa54cbe238684dc1786d8fc1502cf5
Viktor Nareiko <vnareiko.lt@gmail.com>
2020-11-22 14:22:00 +0200

    Example with data rest.

--- src/main/java/com/sd/shop/potatoes/repositories/ProductRepository.java
+++ src/main/java/com/sd/shop/potatoes/repositories/ProductRepository.java
@@ -4,8 +4,6 @@ import com.sd.shop.potatoes.entities.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
     Iterable<Product> findByNameAndPriceGreaterThan(String name, Double price);
