0f63de0d6cdb5537c3f302e67794874bb7ccdd40
Viktor Nareiko <vnareiko.lt@gmail.com>
2020-11-29 11:38:39 +0200

    Added comment.

--- src/main/java/com/sd/shop/potatoes/config/SpringSecurityConfig.java
+++ src/main/java/com/sd/shop/potatoes/config/SpringSecurityConfig.java
@@ -69,6 +69,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
+// This will work for JWT token based authentication
         http.authorizeRequests()
                 .antMatchers("/", "/js/**", "/register", "/css/**", "/images/**", "/api/authenticate").permitAll()
                 .antMatchers("/api/users").hasAuthority("ADMIN")
